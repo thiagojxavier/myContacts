e3dc92d8085be469246289a8f2a892de5f202ccb
Thiago <dejesusxavierthiago@gmail.com>
2025-01-20 11:46:09 -0300

    Created Button, Input, Select components

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,9 +1,25 @@
 import PageHeader from '../../components/PageHeader';
 
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import Button from '../../components/Button';
+
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo contato"
-    />
+    <>
+      <PageHeader
+        title="Novo contato"
+      />
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+      <Button type="button">
+        Salvar alterações
+      </Button>
+    </>
   );
 }
