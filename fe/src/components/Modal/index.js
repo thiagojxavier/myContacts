42560d073ad66983a02cf67ac0f9da62355f67ac
Thiago <dejesusxavierthiago@gmail.com>
2025-01-21 12:15:08 -0300

    Created Modal component

---
+++ fe/src/components/Modal/index.js
@@ -0,0 +1,27 @@
+import { Overlay, Container, Footer } from './styles';
+
+import Button from '../Button';
+
+export default function Modal() {
+  return (
+    <Overlay>
+      <Container>
+        <h1>Título</h1>
+        <p>
+          Corpo do modal
+        </p>
+
+        <Footer>
+          <button type="button" className="cancel-button">
+            Cancelar
+          </button>
+
+          <Button type="button">
+            Deletar
+          </Button>
+
+        </Footer>
+      </Container>
+    </Overlay>
+  );
+}
