653e9bfa7cca1d7720fa1ce793ae52a4084a225d
Thiago <dejesusxavierthiago@gmail.com>
2025-01-21 23:14:11 -0300

    Created loader component

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -11,14 +11,13 @@ import {
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-import Modal from '../../components/Modal';
+import Loader from '../../components/Loader';
+// import Modal from '../../components/Modal';
 
 export default function Home() {
   return (
     <Container>
-
-      <Modal danger />
-
+      <Loader />
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome..." />
       </InputSearchContainer>
