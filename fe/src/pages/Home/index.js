0a52c376b87922cbbd546fe9f29fb5a3d1df4305
Thiago <dejesusxavierthiago@gmail.com>
2025-02-25 12:05:59 -0300

    Infinite loop problem solved with useCallback

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
-import { useEffect, useState, useMemo } from 'react';
+import {
+  useEffect, useState, useMemo, useCallback,
+} from 'react';
 
 import {
   Container,
@@ -31,7 +33,7 @@ export default function Home() {
     contact.name.toLowerCase().includes(searchTerm.toLowerCase())
   )), [contacts, searchTerm]);
 
-  async function loadContacts() {
+  const loadContacts = useCallback(async () => {
     try {
       setIsLoading(true);
 
@@ -44,11 +46,11 @@ export default function Home() {
     } finally {
       setIsLoading(false);
     }
-  }
+  }, [orderBy]);
 
   useEffect(() => {
     loadContacts();
-  }, [orderBy]);
+  }, [loadContacts]);
 
   function handleToggleOrderBy() {
     setOrderBy(
