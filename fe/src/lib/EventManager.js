8dbf1f3572340651e42bd735384026bfcf23e47d
Thiago <dejesusxavierthiago@gmail.com>
2025-02-27 12:26:28 -0300

    Methods for creating listeners and emitting created events

---
+++ fe/src/lib/EventManager.js
@@ -0,0 +1,27 @@
+export default class EventManager {
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event, listener) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event].push(listener);
+  }
+
+  emit(event, payload) {
+    if (!this.listeners[event]) {
+      return;
+    }
+
+    this.listeners[event].forEach((listener) => {
+      listener(payload);
+    });
+  }
+}
+
+const toastEventManager = new EventManager();
+
+console.log(toastEventManager);
