8cb033a28a6e6239a7cf68ed3d58aba2a3a8b8dc
Thiago <dejesusxavierthiago@gmail.com>
2025-03-01 09:56:01 -0300

    Improved react portal

--- fe/src/components/Modal/index.js
+++ fe/src/components/Modal/index.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
 
 import { Overlay, Container, Footer } from './styles';
 
 import Button from '../Button';
+import ReactPortal from '../ReactPortal';
 
 export default function Modal({
   danger, visible, isLoading, title, children, cancelLabel, confirmLabel, onCancel, onConfirm,
@@ -12,28 +12,29 @@ export default function Modal({
     return null;
   }
 
-  return ReactDOM.createPortal(
-    <Overlay>
-      <Container danger={danger}>
-        <h1>{title}</h1>
+  return (
+    <ReactPortal containerId="modal-root">
+      <Overlay>
+        <Container danger={danger}>
+          <h1>{title}</h1>
 
-        <div className="modal-body">
-          {children}
-        </div>
+          <div className="modal-body">
+            {children}
+          </div>
 
-        <Footer>
-          <button type="button" className="cancel-button" onClick={onCancel} disabled={isLoading}>
-            {cancelLabel}
-          </button>
+          <Footer>
+            <button type="button" className="cancel-button" onClick={onCancel} disabled={isLoading}>
+              {cancelLabel}
+            </button>
 
-          <Button type="button" danger={danger} onClick={onConfirm} isLoading={isLoading}>
-            {confirmLabel}
-          </Button>
+            <Button type="button" danger={danger} onClick={onConfirm} isLoading={isLoading}>
+              {confirmLabel}
+            </Button>
 
-        </Footer>
-      </Container>
-    </Overlay>,
-    document.getElementById('modal-root'),
+          </Footer>
+        </Container>
+      </Overlay>
+    </ReactPortal>
   );
 }
 
