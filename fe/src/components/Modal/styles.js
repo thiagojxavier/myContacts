c7690c2b29039f78d280af09eafc4e6ac3104408
Thiago <dejesusxavierthiago@gmail.com>
2025-03-03 20:05:42 -0300

    Animated modal input

--- fe/src/components/Modal/styles.js
+++ fe/src/components/Modal/styles.js
@@ -1,4 +1,24 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+const scaleIn = keyframes`
+  from {
+    transform: scale(0);
+  }
+
+  to {
+    transform: scale(1);
+  }
+`;
 
 export const Overlay = styled.div`
   background: rgba(0, 0, 0, 0.6);
@@ -11,15 +31,17 @@ export const Overlay = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  animation: ${fadeIn} .3s forwards;
 `;
 
 export const Container = styled.div`
   width: 100%;
+  max-width: 450px;
   background: #FFF;
   border-radius: 4px;
   padding: 24px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.4);
-  max-width: 450px;
+  animation: ${scaleIn} 0.3s;
 
   > h1 {
     font-size: 22px;
